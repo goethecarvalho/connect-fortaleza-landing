3bc1e63dca1fcee630c430ca1c3a6d8a58c9050b
Goethe Nascimento <goethecarvalho@gmail.com>
2025-10-22 09:18:21 -0300

    Ajustes no select valor da conta

--- src/app/landing/landing.ts
+++ src/app/landing/landing.ts
@@ -30,28 +30,18 @@ export class LandingComponent {
   };
 
   enviarWhatsApp() {
-    const numeroEmpresa1 = '5585986441000';
-    const numeroEmpresa2 = '5585991172199'; // número alternativo
+    const numeroEmpresa = '5585986441000';
+    //const numeroEmpresa2 = '5585991172199'; // número alternativo
     const numeroLimpo = this.formData.whatsapp.replace(/\D/g, '');
 
     // ✅ Define qual número usar conforme o valor da conta
-    const numeroDestino =
-      this.formData.valorConta === 'Até 300' ? numeroEmpresa2 : numeroEmpresa1;
+    //const numeroDestino = this.formData.valorConta === 'Até 300' ? numeroEmpresa2 : numeroEmpresa1;
 
     // ✅ Monta a mensagem
     const mensagem = `Olá, meu nome é ${this.formData.nome}, sou de ${this.formData.localidade}. Quero economizar em ${this.formData.tipoCliente}. Meu WhatsApp é ${numeroLimpo} e o valor da minha conta de luz é R$ ${this.formData.valorConta}. Gostaria de saber mais sobre energia solar.`;
 
     // ✅ Gera o link do WhatsApp
-    const url = `https://wa.me/${numeroDestino}?text=${encodeURIComponent(mensagem)}`;
-    window.open(url, '_blank');
-  }
-
-
-  /*enviarWhatsApp() {
-    const numeroEmpresa = '5585999964078';
-    const numeroLimpo = this.formData.whatsapp.replace(/\D/g, '');
-    const mensagem = `Olá, meu nome é ${this.formData.nome}, sou de ${this.formData.localidade}. Quero economizar em ${this.formData.tipoCliente}. Meu WhatsApp é ${numeroLimpo} e o valor da minha conta de luz é R$ ${this.formData.valorConta}. Gostaria de saber mais sobre energia solar.`;
     const url = `https://wa.me/${numeroEmpresa}?text=${encodeURIComponent(mensagem)}`;
     window.open(url, '_blank');
-  }*/
+  }
 }
