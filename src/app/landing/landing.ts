94cddbf00d59fc4e41d891d62d09308bc9d5d1c1
Goethe Nascimento <goethecarvalho@gmail.com>
2025-10-21 21:10:13 -0300

    PWA

--- src/app/landing/landing.ts
+++ src/app/landing/landing.ts
@@ -3,11 +3,20 @@ import { FormsModule } from '@angular/forms';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
 import { Beneficios } from "../beneficios/beneficios";
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 
 @Component({
-  selector: 'app-root', // ✅ define onde o Angular deve injetar
+  selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, Header, Footer, Beneficios],
+  imports: [
+    FormsModule,
+    Header,
+    Footer,
+    Beneficios,
+    NgxMaskDirective,
+    NgxMaskPipe
+  ],
+  providers: [provideNgxMask()],
   templateUrl: './landing.html',
   styleUrls: ['./landing.css']
 })
@@ -15,14 +24,15 @@ export class LandingComponent {
   formData = {
     nome: '',
     localidade: '',
+    tipoCliente: '',
     whatsapp: '',
     valorConta: ''
   };
 
   enviarWhatsApp() {
-    //const numeroEmpresa = '5585986441000';
-    const numeroEmpresa = '5585999964078';
-    const mensagem = `Olá, meu nome é ${this.formData.nome}, sou de ${this.formData.localidade}. Meu WhatsApp é ${this.formData.whatsapp} e o valor da minha conta de luz é R$ ${this.formData.valorConta}. Gostaria de saber mais sobre energia solar.`;
+    const numeroEmpresa = '5585991172199';
+    const numeroLimpo = this.formData.whatsapp.replace(/\D/g, '');
+    const mensagem = `Olá, meu nome é ${this.formData.nome}, sou de ${this.formData.localidade}. Quero economizar em ${this.formData.tipoCliente}. Meu WhatsApp é ${numeroLimpo} e o valor da minha conta de luz é R$ ${this.formData.valorConta}. Gostaria de saber mais sobre energia solar.`;
     const url = `https://wa.me/${numeroEmpresa}?text=${encodeURIComponent(mensagem)}`;
     window.open(url, '_blank');
   }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { LandingComponent } from './app/landing/landing';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
-bootstrapApplication(LandingComponent);
+bootstrapApplication(LandingComponent, {
+  providers: [provideServiceWorker('ngsw-worker.js', {
+              enabled: !isDevMode(),
+              registrationStrategy: 'registerWhenStable:30000'
+            })]
+});
