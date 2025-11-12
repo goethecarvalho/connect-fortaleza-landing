c05d7ec96e3f5a252bc99be694b0c0a286ed81a2
Goethe Nascimento <goethecarvalho@gmail.com>
2025-11-12 10:42:06 -0300

    Inclusão do tipo de recurso

--- src/app/landing/landing.ts
+++ src/app/landing/landing.ts
@@ -26,11 +26,13 @@ export class LandingComponent {
     localidade: '',
     tipoCliente: '',
     whatsapp: '',
+    tipoRecurso: '',
     valorConta: ''
   };
 
   enviarWhatsApp() {
-    const numeroEmpresa = '5585986441000';
+    //const numeroEmpresa = '5585986441000';
+    const numeroEmpresa = '5585991172199';
     //const numeroEmpresa2 = '5585991172199'; // número alternativo
     const numeroLimpo = this.formData.whatsapp.replace(/\D/g, '');
 
@@ -38,7 +40,27 @@ export class LandingComponent {
     //const numeroDestino = this.formData.valorConta === 'Até 300' ? numeroEmpresa2 : numeroEmpresa1;
 
     // ✅ Monta a mensagem
-    const mensagem = `Olá, meu nome é ${this.formData.nome}, sou de ${this.formData.localidade}. Quero economizar em ${this.formData.tipoCliente}. Meu WhatsApp é ${numeroLimpo} e o valor da minha conta de luz é R$ ${this.formData.valorConta}. Gostaria de saber mais sobre energia solar.`;
+    //const mensagem = `Olá, meu nome é ${this.formData.nome}, sou de ${this.formData.localidade}. Quero economizar em ${this.formData.tipoCliente}. Tipo de recurso ${this.formData.tipoRecurso}. Meu WhatsApp é ${numeroLimpo} e o valor da minha conta de luz é R$ ${this.formData.valorConta}. Gostaria de saber mais sobre energia solar.`;
+
+    const mensagem = `
+      Olá, meu nome é *${this.formData.nome}*.
+      Sou de *${this.formData.localidade}*.
+      Quero economizar em: *${this.formData.tipoCliente}*.
+      Tipo de recurso: *${this.formData.tipoRecurso}*.
+      Meu WhatsApp é: *${numeroLimpo}*.
+      O valor da minha conta de luz é: R$ *${this.formData.valorConta}*.
+
+      Gostaria de saber mais sobre energia solar.`;
+
+    /*const mensagem = `Olá! 👋  
+      Meu nome é ${this.formData.nome}, sou de ${this.formData.localidade}.  
+
+      Quero economizar em *${this.formData.tipoCliente}* utilizando *${this.formData.tipoRecurso}*.  
+      💬 Meu WhatsApp é ${numeroLimpo}.  
+      💡 O valor médio da minha conta de luz é *R$ ${this.formData.valorConta}*.  
+
+      Gostaria de saber mais sobre as opções de *energia solar*.`;*/
+
 
     // ✅ Gera o link do WhatsApp
     const url = `https://wa.me/${numeroEmpresa}?text=${encodeURIComponent(mensagem)}`;
